96178de57ca47553107037c54dc1b6cd4eba82ea
RohitPawar2707 <rohitpawar7058@gmail.com>
2026-04-22 21:36:31 +0530

    start the patterns type questions

--- Patterns/squarePattern.cpp
+++ Patterns/squarePattern.cpp
@@ -1,10 +1,35 @@
-#include <iostream>
-using namespace std ; 
+// #include <iostream>
+// using namespace std ; 
 
-int main (){
+// int main (){
   
+//      int n = 5 ;
+//      for (int i = 1; i<= n; i++){  // Outer Loop 
+//         for (int j = 1; j<=n; j++){  // Inner Loop 
+//             cout << n << " ":
+//         }
+//         cout << endl ;
+//      }
 
 
+//     return 0 ;
+// }
 
-    return 0 ;
+
+// Print the characters in the square pattern...
+#include <iostream>
+using namespace std;
+
+int main (){ 
+    int n = 10 ;
+    for (int i=0; i<n; i++) {
+        char ch = 'A' ;
+        cout << ch << " ";
+        ch = ch + 1;
+    }
+
+    cout << endl; 
+
 }
+
+ 
